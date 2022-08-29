beb70838582bef782cb8bd3da67fb0ce5f77b964
= <vikas1pandey020@gmail.com>
2022-08-29 21:27:55 +0530

    fourth commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-console.log(process.env);
+
 export default axios.create({
-  baseURL: "http://api.weatherapi.com/v1/",
+  baseURL: "https://api.weatherapi.com/v1/",
   headers: {
     key: API_KEY,
   },
